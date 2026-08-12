47c6f5de3bea808c13da82f45c174143a9323914
Hustler031 <106297411+Hustler031@users.noreply.github.com>
2026-08-12 21:21:47 +0530

    Add dynamic source module boundary

---
+++ js/sources.js
@@ -0,0 +1,5 @@
+(() => {
+  // Dynamic categories and PDF/source selectors will be populated from the Sources API.
+  // New PDFs therefore require database registration, not frontend rewrites.
+  window.EnglishSources = Object.freeze({});
+})();
