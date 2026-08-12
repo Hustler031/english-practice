3c79530c3f5dc482cb5d6045eda9b985c236eedb
Hustler031 <106297411+Hustler031@users.noreply.github.com>
2026-08-12 21:21:33 +0530

    Add quiz module boundary

---
+++ js/quiz.js
@@ -0,0 +1,5 @@
+(() => {
+  // Quiz session logic will live here.
+  // Keeping it isolated prevents quiz UI changes from affecting API/database code.
+  window.EnglishQuiz = Object.freeze({});
+})();
